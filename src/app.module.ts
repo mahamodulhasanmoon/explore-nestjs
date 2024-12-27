2e1a00cd281925cdf6579d9bf5c668bdd4a78883
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-12-27 22:00:07 +0600

    RATE LIMIING ADDED

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,28 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeeModule } from './employee/employee.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule, DatabaseModule, EmployeeModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 5,
+      },
+    ]),
+    UserModule,
+    DatabaseModule,
+    EmployeeModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
