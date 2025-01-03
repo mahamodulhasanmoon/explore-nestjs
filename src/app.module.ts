34935ebfb8fd1da16252d9bdb417585c5f7c7ea1
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2025-01-03 19:38:13 +0600

    interceptor added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { DatabaseModule } from './database/database.module';
 import { EmployeeModule } from './employee/employee.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { APP_GUARD } from '@nestjs/core';
     UserModule,
     DatabaseModule,
     EmployeeModule,
+    LoggerModule,
   ],
   controllers: [AppController],
   providers: [
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerService } from './logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(LoggerService));
   app.setGlobalPrefix('api/v1');
   app.enableCors();
   app.useGlobalPipes(
